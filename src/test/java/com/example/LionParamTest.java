78a0a7a343db451d9e400168ba7adfc4007c90da
n.fursov <n.fursov@vkteam.ru>
2026-05-15 17:42:11 +0300

    replaced spy with mock from LionParamTest

--- src/test/java/com/example/LionParamTest.java
+++ src/test/java/com/example/LionParamTest.java
@@ -3,8 +3,9 @@ package com.example;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,14 +13,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(MockitoExtension.class)
 public class LionParamTest {
 
-    @Spy
-    Feline spyFeline = new Feline();
+    @Mock
+    Feline mockFeline;
+
 
     @ParameterizedTest
     @CsvSource({"2", "3", "5"})
     void testGetKittensReturnsExpectedKittensCount(int kittens) throws Exception {
-        Mockito.when(spyFeline.getKittens()).thenReturn(kittens);
-        Lion lion = new Lion("Самец", spyFeline);
+        Mockito.doReturn(kittens).when(mockFeline).getKittens();
+        Lion lion = new Lion("Самец", mockFeline);
         assertEquals(kittens, lion.getKittens());
     }
 
@@ -29,7 +31,7 @@ public class LionParamTest {
             "Самка, false"
     })
     void testDoesHaveManeReturnsExpectedValue(String sex, boolean expectedHasMane) throws Exception {
-        Lion lion = new Lion(sex, spyFeline);
+        Lion lion = new Lion(sex, mockFeline);
         assertEquals(expectedHasMane, lion.doesHaveMane());
     }
 
